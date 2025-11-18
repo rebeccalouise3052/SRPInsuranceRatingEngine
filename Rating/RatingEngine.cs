1b07f6c4dc580d48160618c9a597adb3e1538d45
Rebecca Stephens <rebecca.stephens@intellica.co.uk>
2025-11-18 14:14:50 +0000

    Initial commit

---
+++ Rating/RatingEngine.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.IO;
+
+namespace ArdalisRating
+{
+    /// <summary>
+    /// The RatingEngine reads the policy application details from a file and produces a numeric 
+    /// rating value based on the details.
+    /// </summary>
+    public class RatingEngine
+    {
+        public decimal Rating { get; set; } // defines a decimal property named rating, where final calculated insurance rating is stored
+                                            // { get; set; } get: allow other code to read the value, set: allow other code to assign a value
+        public void Rate() // main method that performs the entire rating process
+        {
+            Console.WriteLine("Starting rate."); // print a message to indicate that rating has begun
+
+            Console.WriteLine("Loading policy."); 
+
+            // load policy - open file policy.json
+            string policyJson = File.ReadAllText("policy.json"); // reads the entire contents of polcy.json into a string. File.ReadAllText expects the file to be in the working directory
+
+            var policy = JsonConvert.DeserializeObject<Policy>(policyJson,
+                new StringEnumConverter()); // converts policyJson into a Policy object. USing StringEnumConverter so JSON strings like "auto" convert to the enum PolicyType.Auto
+
+            switch (policy.Type) // Switches on the PolicyType enum, possible values: Auto, Land, Life
+            {
+                case PolicyType.Auto:
+                    Console.WriteLine("Rating AUTO policy...");
+                    Console.WriteLine("Validating policy.");
+                    if (String.IsNullOrEmpty(policy.Make))
+                    {
+                        Console.WriteLine("Auto policy must specify Make");
+                        return;
+                    }
+                    if (policy.Make == "BMW")
+                    {
+                        if (policy.Deductible < 500)
+                        {
+                            Rating = 1000m;
+                        }
+                        else Rating = 900m;
+                    }
+                    break;
+
+                case PolicyType.Land:
+                    Console.WriteLine("Rating LAND policy...");
+                    Console.WriteLine("Validating policy.");
+                    if (policy.BondAmount == 0 || policy.Valuation == 0)
+                    {
+                        Console.WriteLine("Land policy must specify Bond Amount and Valuation.");
+                        return;
+                    }
+                    if (policy.BondAmount < 0.8m * policy.Valuation)
+                    {
+                        Console.WriteLine("Insufficient bond amount.");
+                        return;
+                    }
+                    Rating = policy.BondAmount * 0.05m;
+                    break;
+
+                case PolicyType.Life:
+                    Console.WriteLine("Rating LIFE policy...");
+                    Console.WriteLine("Validating policy.");
+                    if (policy.DateOfBirth == DateTime.MinValue)
+                    {
+                        Console.WriteLine("Life policy must include Date of Birth.");
+                        return;
+                    }
+                    if (policy.DateOfBirth < DateTime.Today.AddYears(-100))
+                    {
+                        Console.WriteLine("Centenarians are not eligible for coverage.");
+                        return;
+                    }
+                    if (policy.Amount == 0)
+                    {
+                        Console.WriteLine("Life policy must include an Amount.");
+                        return;
+                    }
+                    int age = DateTime.Today.Year - policy.DateOfBirth.Year;
+                    if (policy.DateOfBirth.Month == DateTime.Today.Month &&
+                        DateTime.Today.Day < policy.DateOfBirth.Day ||
+                        DateTime.Today.Month < policy.DateOfBirth.Month)
+                    {
+                        age--;
+                    }
+                    decimal baseRate = policy.Amount * age / 200;
+                    if (policy.IsSmoker)
+                    {
+                        Rating = baseRate * 2;
+                        break;
+                    }
+                    Rating = baseRate;
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown policy type");
+                    break;
+            }
+
+            Console.WriteLine("Rating completed.");
+        }
+    }
+}
